d34043dbcb53adcd043e8c2bd33fc03a192ec1e0
RegularSvensson <elias.svensson.1992@gmail.com>
2015-03-31 18:57:30 +0200

    Add model info to ViewModel

--- js/app.js
+++ js/app.js
@@ -1,2 +1,7 @@
 var ViewModel = function() {
+	this.clickCount = ko.observable(0);
+	this.name = ko.observable('Tabby');
+	this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
+	this.imgAttribution = ko.observable('www.google.com');
+
 }
