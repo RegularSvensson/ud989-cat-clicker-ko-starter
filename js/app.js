81f446087b3a5c2444551e14459e7f8f32e06d28
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-01 14:25:08 +0200

    Add Cat function to store cat object data in DOM later

--- js/app.js
+++ js/app.js
@@ -16,6 +16,28 @@ var initialCats = [
 
 ]
 
+var Cat = function(data) {
+	this.clickCount = ko.observable(data.clickCount);
+	this.name = ko.observable(data.name);
+	this.imgSrc = ko.observable(data.imgSrc);
+	this.imgAttribution = ko.observable(data.imgAttribution);
+	this.nicknames = ko.observableArray(data.nicknames);
+	
+	this.title = ko.computed(function() {
+		var title;
+		var clicks = this.clickCount();
+		if (clicks < 10) {
+			title = 'Newborn';
+		} else if (clicks < 50) {
+			title = 'Infant';
+		} else {
+			title = 'Master of the universe!!';
+		}
+		return title;
+	}, this);
+}
+
+
 var ViewModel = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
