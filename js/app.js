6e6fb0641048fac7e0d10d4c4bc0ddbf91785437
RegularSvensson <elias.svensson.1992@gmail.com>
2015-04-01 14:23:25 +0200

    Add array of cat objects

--- js/app.js
+++ js/app.js
@@ -1,3 +1,21 @@
+var initialCats = [
+	{
+		clickCount: 0,
+		name: 'Tabby',
+		imgSrc: 'img/434164568_fea0ad4013_z.jpg',
+		imgAttribution: 'www.google.com',
+		nicknames: ['Tabtab', 'T-Bone', 'T-tip']
+	},
+	{
+		clickCount: 0,
+		name: 'Tabby2',
+		imgSrc: 'img/22252709_010df3379e_z.jpg',
+		imgAttribution: 'www.google.com',
+		nicknames: ['Tabtab2', 'T-Bone2', 'T-tip2']	
+	},
+
+]
+
 var ViewModel = function() {
 	this.clickCount = ko.observable(0);
 	this.name = ko.observable('Tabby');
